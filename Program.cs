0723a85802bd1cf5ccafb84e79d55254d40bf77a
Varvara <frommars.2013@mail.ru>
2022-12-04 13:24:47 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
 ﻿
 //Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-Console.WriteLine("Введите трёхзначное число");
+//Console.WriteLine("Введите трёхзначное число");
+//int a = Convert.ToInt32(Console.ReadLine());
+//int b=a/10;
+//int c=b%10;
+//Console.WriteLine("Вторая цифра  ");
+//Console.WriteLine(c);
 
+//Напишите программу, которая с помощью деления выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 645 -> 5, 78 -> третьей цифры нет, 32679 -> 6
+
+Console.WriteLine("Введите число");
 int a = Convert.ToInt32(Console.ReadLine());
-
-int b=a/10;
-int c=b%10;
-
-Console.WriteLine("Вторая цифра  ");
-
-Console.WriteLine(c);
+int b = a % 100;
+int c = b % 10;
+if (a >= 100)
+{
+  Console.WriteLine("третья цифра -> " + c);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
