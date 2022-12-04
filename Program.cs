2df29f7d372621e87bd5f74cc71e5ef758e17386
Varvara <frommars.2013@mail.ru>
2022-12-04 12:35:14 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трёхзначное число");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int b=a/10;
+int c=b%10;
+
+Console.WriteLine("Вторая цифра  ");
+
+Console.WriteLine(c);
